0e56e0f5bcb90cf9de1c4df1decbec8c1c8b7fe6
Ameet Jayawant <ameetj85@gmail.com>
2026-03-12 10:43:06 -0400

    added bloomberg custom agent

---
+++ how-to/custom-agents/bloomberg-agent/bbg-tcapi/client/src/shapes.ts
@@ -0,0 +1,50 @@
+/**
+ * Interface for a logger.
+ */
+export interface Logger {
+	/**
+	 * Log data as information.
+	 * @param message The message to log.
+	 * @param optionalParams Optional parameters for details.
+	 */
+	info(message: unknown, ...optionalParams: unknown[]): void;
+
+	/**
+	 * Log data as error.
+	 * @param message The message to log.
+	 * @param optionalParams Optional parameters for details.
+	 */
+	error(message: unknown, ...optionalParams: unknown[]): void;
+
+	/**
+	 * Log data as warning.
+	 * @param message The message to log.
+	 * @param optionalParams Optional parameters for details.
+	 */
+	warn(message: unknown, ...optionalParams: unknown[]): void;
+
+	/**
+	 * Log data as trace.
+	 * @param message The message to log.
+	 * @param optionalParams Optional parameters for details.
+	 */
+	trace(message: unknown, ...optionalParams: unknown[]): void;
+
+	/**
+	 * Log data as debug.
+	 * @param message The message to log.
+	 * @param optionalParams Optional parameters for details.
+	 */
+	debug(message: unknown, ...optionalParams: unknown[]): void;
+}
+
+/**
+ * Interface for the search agent configuration data.
+ */
+export interface BloombergAgentSettings {
+	[key: string]: unknown;
+	/**
+	 * Bloomberg Terminal API key for authentication.
+	 */
+	bbgAPIKey: string;
+}
